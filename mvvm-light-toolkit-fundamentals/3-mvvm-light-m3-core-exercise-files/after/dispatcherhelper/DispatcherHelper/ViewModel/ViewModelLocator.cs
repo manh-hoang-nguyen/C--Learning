8489d6bda5ebb4145538b2b3c9896def28d8111b
Manh-Hoang <nguyenhoang56ksgt@gmail.com>
2020-07-02 07:45:19 +0200

    update

---
+++ mvvm-light-toolkit-fundamentals/3-mvvm-light-m3-core-exercise-files/after/dispatcherhelper/DispatcherHelper/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,38 @@
+﻿using DispatcherHelperSample.Model;
+using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+
+namespace DispatcherHelperSample.ViewModel
+{
+    public class ViewModelLocator
+    {
+        static ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            SimpleIoc.Default.Register<ISensorService, SensorService>();
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        /// <summary>
+        /// Gets the Main property.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance",
+            "CA1822:MarkMembersAsStatic",
+            Justification = "This non-static member is needed for data binding purposes.")]
+        public MainViewModel Main
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MainViewModel>();
+            }
+        }
+
+        /// <summary>
+        /// Cleans up all the resources.
+        /// </summary>
+        public static void Cleanup()
+        {
+        }
+    }
+}
---
+++ mvvm-light-toolkit-fundamentals/4-mvvm-light-m4-extras-exercise-files/after/simpleioc/WhyMvvm/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+using WhyMvvm.Design;
+using WhyMvvm.Helpers;
+using WhyMvvm.Model;
+
+namespace WhyMvvm.ViewModel
+{
+    public class ViewModelLocator
+    {
+        public static ViewModelLocator Instance
+        {
+            get
+            {
+                return Application.Current.Resources["Locator"] as ViewModelLocator;
+            }
+        }
+
+        public MainViewModel Main
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MainViewModel>();
+            }
+        }
+
+        public ViewModelLocator()
+        {
+            if (ViewModelBase.IsInDesignModeStatic)
+            {
+                SimpleIoc.Default.Register<IFriendsService, DesignFriendsService>();
+            }
+            else
+            {
+                SimpleIoc.Default.Register<IFriendsService, FriendsService>();
+            }
+
+            SimpleIoc.Default.Register<IDialogService, DialogService>();
+            SimpleIoc.Default.Register<INavigationService, NavigationService>();
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+    }
+}
